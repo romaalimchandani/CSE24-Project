ccbd298851c07879ec92398d7303698d3b5300ff
Roma Alimchandani <65796150+romaalimchandani@users.noreply.github.com>
2025-05-06 11:25:21 -0700

    Update Point.h

--- src/Point.h
+++ src/Point.h
@@ -4,11 +4,7 @@
 #include "Shape.h"
 
 class Point : public Shape {
-    float x;
-    float y;
-    float r;
-    float g;
-    float b;
+    float x, y, r, g, b;
     int size;
 
 public:
@@ -18,17 +14,16 @@ public:
     Point(float x, float y, float r, float g, float b, int size);
 
     void draw();
-    
-
     float getX() const;
     float getY() const;
     float getR() const;
     float getG() const;
     float getB() const;
     int getSize() const;
-
     bool contains(float mx, float my);
     void setColor(float r, float g, float b);
+    void moveBy(float dx, float dy);
+    void resize(float sizeFactor);
 };
 
-#endif
+#endif // POINT_H
