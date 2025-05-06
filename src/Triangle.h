65fabbb9a654a93bf31dec4589852888c712753a
Roma Alimchandani <65796150+romaalimchandani@users.noreply.github.com>
2025-05-06 11:34:55 -0700

    Update Triangle.h

--- src/Triangle.h
+++ src/Triangle.h
@@ -4,20 +4,24 @@
 #include "Shape.h"
 
 class Triangle : public Shape {
-    float x;
-    float y;
-    float base;
-    float height;
-    float r;
-    float g;
-    float b;
+private:
+    float x = 0.0f;
+    float y = 0.0f;
+    float base = 0.4f;
+    float height = 0.4f;
+    float r = 0.0f;
+    float g = 0.0f;
+    float b = 0.0f;
 
 public:
     Triangle();
     Triangle(float x, float y, float base, float height, float r, float g, float b);
-    void draw();
-    bool contains(float mx, float my);
-    void setColor(float r, float g, float b);
+
+    void draw() override;
+    bool contains(float mx, float my) override;
+    void setColor(float r, float g, float b) override;
+    void moveBy(float dx, float dy) override;
+    void resize(float sizeFactor) override;
 
     friend struct AppTest;
 };
