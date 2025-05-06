492c9fa14506bfa4e639911b1ebf32c519e3ec7f
Roma Alimchandani <65796150+romaalimchandani@users.noreply.github.com>
2025-05-06 11:12:11 -0700

    Update Canvas.h

--- src/Canvas.h
+++ src/Canvas.h
@@ -3,36 +3,37 @@
 
 #include <bobcat_ui/all.h>
 #include <vector>
-#include "Point.h"
+#include "Shape.h"
+#include "Scribble.h"
 #include "Rectangle.h"
 #include "Circle.h"
 #include "Triangle.h"
 #include "Polygon.h"
-#include "Shape.h"
-#include "Scribble.h"
 
 class Canvas : public bobcat::Canvas_ {
-    std::vector<Point*> points;
-    std::vector<Shape*> shapes;
+private:
+    std::vector<Shape*> objects;
 
 public:
+    float lastClickX;
+    float lastClickY;
+
     Canvas(int x, int y, int w, int h);
 
+    void addShape(Shape* shape); // For scribbles and other tools
     void addPoint(float x, float y, float r, float g, float b, int size);
-
     void addRectangle(float x, float y, float r, float g, float b);
-
     void addCircle(float x, float y, float r, float g, float b);
-
     void addTriangle(float x, float y, float r, float g, float b);
-
     void addPolygon(float x, float y, float r, float g, float b);
 
     void clear();
-
     void render();
+    void bringToFront();
+    void sendToBack();
 
     Shape* getSelectedShape(float mx, float my);
+    const std::vector<Shape*>& getShapes() const;
 };
 
 #endif
