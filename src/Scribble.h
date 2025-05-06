49e3ef9eafab7c18efa25df6926a29da1c7e7a15
Roma Alimchandani <65796150+romaalimchandani@users.noreply.github.com>
2025-05-06 11:31:10 -0700

    Update Scribble.h

--- src/Scribble.h
+++ src/Scribble.h
@@ -5,15 +5,21 @@
 #include "Point.h"
 #include <vector>
 
-class Scribble: public Shape {
+class Scribble : public Shape {
+private:
     std::vector<Point*> points;
 
 public:
+    Scribble() = default;
+    ~Scribble();
 
     void addPoint(float x, float y, float r, float g, float b, int size);
 
-    void draw();
-
-    ~Scribble();
+    void draw() override;
+    bool contains(float mx, float my) override;
+    void setColor(float r, float g, float b) override;
+    void moveBy(float dx, float dy) override;
+    void resize(float size) override {} // Not applicable for scribble
 };
+
 #endif
