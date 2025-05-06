10a6d2213c6362159a969640b44a7d51b83871a0
Roma Alimchandani <65796150+romaalimchandani@users.noreply.github.com>
2025-05-06 11:29:05 -0700

    Update Polygon.h

--- src/Polygon.h
+++ src/Polygon.h
@@ -18,6 +18,8 @@ public:
     void draw();
     bool contains(float mx, float my);
     void setColor(float r, float g, float b);
+    void moveBy(float dx, float dy);
+    void resize(float size);
 
     friend struct AppTest;
 };
